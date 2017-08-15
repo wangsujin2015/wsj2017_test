d3d7285e126e034e2f7f0217897a4a2252e9d6e2
王苏进 <598837443@qq.com>
2017-08-15 14:47:55 +0800

    初始化

---
+++ WSJ2017_test/WSJ2017_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSJ2017_test
+//
+//  Created by 李旭 on 2017/8/15.
+//  Copyright © 2017年 李旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
